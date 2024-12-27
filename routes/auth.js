283a3aa6548f40cfc251d9ca01e1fdfdad82c82c
Kaushal Kishor Rauniyar <157037723+kaushalxluciferr@users.noreply.github.com>
2024-12-28 00:20:19 +0530

    Updated code with fixes for Vercel deployment

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const router=require('express').Router()
 
 const User=require('../model/user')
-const bcrypt=require('bcrypt')
+const bcrypt = require('bcryptjs');
+
 // for sighninn purpose
 
 router.post("/register",async(req,res)=>{
