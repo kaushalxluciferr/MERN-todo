dd5829a8976741da251136e727985138d110f5d8
Kaushal Kishor Rauniyar <157037723+kaushalxluciferr@users.noreply.github.com>
2024-12-28 00:11:15 +0530

    Updated code with fixes for Vercel deployment

--- connection/conn.js
+++ connection/conn.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const conn=async (req,res)=>{
     try{
-await mongoose.connect(process.env.LINK).then(()=>{
+await mongoose.connect('mongodb+srv://user:user123@todocluster.r5znv.mongodb.net/mydatabase').then(()=>{
     console.log("connect sucessfully");
     
 })
